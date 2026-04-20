004e75e185ee706b970c715663841fd322f4b06e
unknown <szymanskifilip46@gmail.com>
2026-04-20 22:00:59 +0200

    initialized project

---
+++ Wypożyczalnia-filmow/Form1.cs
@@ -0,0 +1,20 @@
+namespace Wypożyczalnia_filmow
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
